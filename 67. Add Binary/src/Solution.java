bdec06a12909bd7b623fd53480363f911d5f234e
Immortan <2633866263@qq.com>
2021-09-06 12:15:27 +0800

    update

--- 67. Add Binary/src/Solution.java
+++ 67. Add Binary/src/Solution.java
@@ -1,59 +1,42 @@
 /*Given two binary strings a and b, return their sum as a binary string.*/
 
-import java.util.Stack;
-
 class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.addBinary("1", "111"));
+    }
+
     public String addBinary(String a, String b) {
-        Stack<Integer> stk1 = new Stack<>();
-        Stack<Integer> stk2 = new Stack<>();
-        Stack<Integer> stk = new Stack<>();
+        StringBuilder s = new StringBuilder();
+        int right1 = a.length() - 1;
+        int right2 = b.length() - 1;
+        int forward = 0;
 
-        for(int i=0; i<a.length(); i++)
-            stk1.push(Integer.valueOf(a.substring(i, i+1)));
-        for(int i=0; i<b.length(); i++)
-            stk2.push(Integer.valueOf(b.substring(i, i+1)));
-
-        while(!stk1.empty() && !stk2.empty()) {
-            int temp = stk1.pop() + stk2.pop();
-            if(temp > 1) {
-                temp -= 2;
-                stk.push(temp);
-                if(stk1.empty())
-                    stk1.push(1);
-                else
-                    stk1.push(stk1.pop() + 1);
-            }
+        while(right1 >= 0 && right2 >= 0) {
+            int temp = a.charAt(right1) + b.charAt(right2) - 2*'0' + forward;
+            forward = temp / 2;
+            s.append(temp % 2);
+            right1--;
+            right2--;
         }
 
-        while(!stk1.empty()) {
-            int temp = stk1.pop();
-            if(temp > 1) {
-                temp -= 2;
-                stk.push(temp);
-                if(stk1.empty())
-                    stk1.push(1);
-                else
-                    stk1.push(stk1.pop() + 1);
-            }
+        while(right1 >= 0) {
+            int temp = a.charAt(right1) - '0' + forward;
+            forward = temp / 2;
+            s.append(temp % 2);
+            right1--;
         }
 
-        while(!stk2.empty()) {
-            int temp = stk2.pop();
-            if(temp > 1) {
-                temp -= 2;
-                stk.push(temp);
-                if(stk2.empty())
-                    stk2.push(1);
-                else
-                    stk2.push(stk1.pop() + 1);
-            }
+        while(right2 >= 0) {
+            int temp = b.charAt(right2) - '0' + forward;
+            forward = temp / 2;
+            s.append(temp % 2);
+            right2--;
         }
 
-        String s = "";
+        if(forward > 0)
+            s.append(forward);
 
-        while(!stk.empty())
-            s = s + stk.pop();
-
-        return s;
+        return s.reverse().toString();
     }
 }
