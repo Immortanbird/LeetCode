639b85af972e8a1db1e95064ce2a2b8d47926332
Immortan <2633866263@qq.com>
2021-09-14 14:44:12 +0800

    first commit

--- 447. Number of Boomerangs/src/Solution.java
+++ 447. Number of Boomerangs/src/Solution.java
@@ -3,8 +3,25 @@
 * Return the number of boomerangs.
 * */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int numberOfBoomerangs(int[][] points) {
+        int res = 0;
 
+        for (int[] x : points) {
+            Map<Integer, Integer> hashmap = new HashMap<>();
+
+            for (int[] y : points) {
+                int dist = (x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]);
+                hashmap.put(dist, hashmap.getOrDefault(dist, 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : hashmap.entrySet())
+                res += entry.getValue() * (entry.getValue() - 1);
+        }
+
+        return res;
     }
 }
