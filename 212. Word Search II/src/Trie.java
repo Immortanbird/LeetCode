c0ac20393e1ec2d4f0f5b1148d0ed8ef1ba2ecf1
Immortan <2633866263@qq.com>
2021-09-16 16:25:23 +0800

    first commit

---
+++ 212. Word Search II/src/Trie.java
@@ -0,0 +1,26 @@
+class Trie {
+    private String word;
+    private boolean isEnd;
+    private final Trie[] next;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        word = "";
+        isEnd = false;
+        next = new Trie[26];
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie temp = this;
+
+        for (int i=0; i<word.length(); i++) {
+            if (temp.next[word.charAt(i) - 'a'] == null)
+                temp.next[word.charAt(i) - 'a'] = new Trie();
+            temp = temp.next[word.charAt(i) - 'a'];
+        }
+
+        temp.isEnd = true;
+        temp.word = word;
+    }
+}
