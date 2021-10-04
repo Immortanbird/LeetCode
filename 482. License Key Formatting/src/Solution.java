72cf1266fe7dadcf3edb08e5e81435b070c2c5e6
Immortan <2633866263@qq.com>
2021-10-04 11:54:10 +0800

    first commit

---
+++ 482. License Key Formatting/src/Solution.java
@@ -0,0 +1,34 @@
+/*
+* You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+*
+* We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+*
+* Return the reformatted license key.
+* */
+
+class Solution {
+    public static void main(String[] main) {
+        System.out.println(Solution.licenseKeyFormatting("5F3Z-2e-9-w", 4));
+    }
+    public static String licenseKeyFormatting(String s, int k) {
+        StringBuilder builder = new StringBuilder();
+
+        int index = 0;
+
+        for (int i=s.length() - 1; i>=0; i--) {
+            if (s.charAt(i) == '-')
+                continue;
+
+            builder.append(s.charAt(i));
+            index ++;
+
+            if (index % k == 0)
+                builder.append('-');
+        }
+
+        if (!builder.isEmpty() && builder.charAt(builder.length() - 1) == '-')
+            builder.deleteCharAt(builder.length() - 1);
+
+        return builder.reverse().toString().toUpperCase();
+    }
+}
