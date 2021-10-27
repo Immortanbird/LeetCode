e544731c20737f5b406fa2e78e300b7d29e73b8b
Immortan <2633866263@qq.com>
2021-10-27 22:12:28 +0800

    first commit

---
+++ 496. Next Greater Element I/src/Solution.java
@@ -0,0 +1,40 @@
+/*
+* The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+* You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+*
+* For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+*
+* Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+* */
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i])
+                stack.pop();
+
+            if (!stack.isEmpty())
+                map.put(nums2[i], stack.peek());
+            else
+                map.put(nums2[i], -1);
+
+            stack.push(nums2[i]);
+        }
+
+        for (int i=0; i<nums1.length; i++)
+            res[i] = map.get(nums1[i]);
+
+        return res;
+    }
+}
